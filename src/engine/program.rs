376f0d9e238de57c29dce7de8ac49d21c8355798
Eric <jhonjhonjumaday@gmail.com>
2023-08-05 15:45:02 +0800

    added method to execute the program

--- src/engine/program.rs
+++ src/engine/program.rs
@@ -4,3 +4,19 @@ pub struct BFFProgram {
     pub version: (u16, u16, u16),
     pub instructions: Vec<Instruction>
 }
+
+
+impl BFFProgram {
+    pub fn new(version: (u16, u16, u16), instructions: Vec<Instruction>) -> Self {
+        Self {
+            version,
+            instructions,
+        }
+    }
+
+    pub fn execute(&mut self) {
+        let mut vm = crate::engine::virtual_machine::VirtualMachine::new();
+        vm.load_program(self.instructions.clone());
+        vm.execute_instruction_list();
+    }
+}
