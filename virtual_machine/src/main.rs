fb6eb79723860fc41315430358d38a79744e35ae
Eric <jhonjhonjumaday@gmail.com>
2023-08-05 23:05:37 +0800

    code restructure, made virtual_machine as the default bin

--- virtual_machine/src/main.rs
+++ virtual_machine/src/main.rs
@@ -1,3 +1,31 @@
+use std::collections::vec_deque::VecDeque;
+use bff;
+
 fn main() {
-    println!("Hello, world!");
+    let mut args: VecDeque<String> = std::env::args().collect();
+    let _path = args.pop_front().unwrap();
+    let mut file_path = "main.bfo".to_string();
+    let mut debug = false;
+
+    if args.len() > 0 && !args[0].starts_with("-"){
+        file_path = args.pop_front().unwrap();
+        if !file_path.ends_with(".bfo") {
+            println!("Warning: Given File is not a '.bfo' file");
+        }
+    }
+
+    while args.len() != 0 {
+        let current_arg = args.pop_front().unwrap();
+
+        if current_arg.starts_with("-"){
+            match &*current_arg {
+                "--debug" | "-d" => {
+                    debug = true;
+                }
+                unknown => {
+                    unimplemented!("Unknown Argument: '{}'", unknown)
+                }
+            }
+        }
+    }
 }
