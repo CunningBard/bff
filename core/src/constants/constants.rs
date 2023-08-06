11133ce81d9fb235b896dba69cb825b1f80d2aab
Eric <jhonjhonjumaday@gmail.com>
2023-08-06 17:18:29 +0800

    clarity on what STACK_POINTER means

--- core/src/constants/constants.rs
+++ core/src/constants/constants.rs
@@ -5,6 +5,6 @@ pub const GENERAL_PURPOSE_REGISTER_COUNT: usize = 16;
 pub const REGISTER_COUNT: usize = GENERAL_PURPOSE_REGISTER_COUNT + 1; // +1 for stack pointer
 pub const BASE_MEMORY_SIZE: usize = 1024 * 1024 / size_of::<Byte>(); // 1 MB
 pub const BASE_STACK_SIZE: usize = 1024 * 1024 / size_of::<Bits>(); // 1 MB
-pub const STACK_POINTER: Register = 0;
+pub const STACK_POINTER: Register = 0; // Register 0 is the stack pointer
 pub const INSTRUCTION_SIZE: u32 = 8;
 pub const INSTRUCTION_COUNT: usize = 95;
