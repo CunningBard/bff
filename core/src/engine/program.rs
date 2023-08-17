dff494f702e8f1e66675a55a5dfab3a1b40b64c7
Eric <jhonjhonjumaday@gmail.com>
2023-08-18 00:19:27 +0800

    made BFFProgram able to compile to bytes, for easier compilation

--- core/src/engine/program.rs
+++ core/src/engine/program.rs
@@ -19,4 +19,16 @@ impl BFFProgram {
         vm.load_program(self.instructions.clone());
         vm.execute_instruction_list();
     }
+
+    pub fn to_bfo_bytes(&self) -> Vec<u8> {
+        let mut bytes = vec![];
+        bytes.extend_from_slice(&self.version.0.to_le_bytes());
+        bytes.extend_from_slice(&self.version.1.to_le_bytes());
+        bytes.extend_from_slice(&self.version.2.to_le_bytes());
+        bytes.extend_from_slice(&[0, 0]);
+        for instruction in &self.instructions {
+            bytes.extend_from_slice(&instruction.to_bfo_bytes());
+        }
+        bytes
+    }
 }
