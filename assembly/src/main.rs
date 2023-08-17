fcba0928695f7fe61683b21b969313f8ed4c8331
Eric <jhonjhonjumaday@gmail.com>
2023-08-18 00:16:09 +0800

    added integrity check and compilation

--- assembly/src/main.rs
+++ assembly/src/main.rs
@@ -5,12 +5,20 @@ extern crate pest_derive;
 extern crate pest;
 
 use std::fs;
+use bffcore::engine::program::BFFProgram;
+use bffcore::engine::virtual_machine::VirtualMachine;
 
 fn main(){
     let mut parser = parser::BffAsmParser::new();
     let contents = &*fs::read_to_string("./assembly/main.bffasm").expect("Couldnt read file");
     parser.parse(contents).expect("Couldnt parse file");
-    for instruction in parser.instructions {
-        println!("{:?}", instruction);
-    }
+
+    let mut bfo_program = BFFProgram::new((0, 1, 0), parser.instructions);
+
+    // write as bfo file
+    let compiled = bfo_program.to_bfo_bytes();
+    println!("Number of instructions: {:?}", compiled.len());
+    assert_eq!(compiled.len() % 8, 0, "Number of instructions is not a multiple of 8");
+
+    fs::write("./assembly/main.bfo", compiled).expect("Couldnt write file");
 }
